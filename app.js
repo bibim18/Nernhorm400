c3d681fa728f38ecd186def19d01c31316029f2a
bibim18 <namfon_inuyasha@hotmail.com>
2019-01-08 22:26:48 +0700

    Add field user follow to id and install nodemon

--- app.js
+++ app.js
@@ -1,69 +1,86 @@
 var express = require('express');
 var fs = require('fs')
-var bodyParser = require("body-parser")
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+var bodyParser = require("body-parser");
+app = express();
 app.use(bodyParser.json())
-
-app.get('/',function(req,res) {
-  res.send('Sample Code for RESTful API')
-})
-
-app.get('/listUsers', function(req,res){
-  fs.readFile(__dirname + "/bin/" + "users.json", 'utf8', function(err,data){
-    console.log(data)
+app.get('/listUsers', function (req, res) {
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
     res.end(data)
   })
+});
+app.get('/showbyID/:id', function (req, res) {
+  var id = parseInt(req.params.id);
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, data) {
+
+    var obj = JSON.parse(data);
+    finaldata = Object.values(obj);
+
+    show = finaldata.filter(dd => dd.id === id)
+    console.log(show[0]);
+
+    showfinal = {};
+
+    showfinal[`user${id}`] = show[0];
+    console.log(showfinal);
+    res.send(showfinal);
+    res.end();
+  });
+
+
+});
+app.post('/addMultiUser', function (req, res) {
+  // First read existing users.
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, dataFile) {
+
+    var data = JSON.parse(dataFile);
+    var count = Object.keys(data).length;
+    var dataUser = req.body
+
+    dataUser.map((user, index) => {
+      data[`user${count + index + 1}`] = {
+        name: user.name,
+        password: user.password,
+        profession: user.profession,
+        id: count + (index + 1)
+      }
+      return data
+    })
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.post('/addUser', function (req, res) {
+  // First read existing users.
+  fs.readFile(__dirname + "/" + "users.json", 'utf8', function (err, dataFile) {
+    var data = JSON.parse(dataFile);
+    var count = Object.keys(data).length;
+    var dataUser = req.body
+    dataUser.id = count + 1
+    data[`user${count + 1}`] = dataUser
+
+    res.end(JSON.stringify(data));
+  });
 })
 
-app.post('/addUser', function(req,res){
-  var json = req.body
-  fs.readFile(__dirname + "/bin/" + "users.json", 'utf8', function(err,data){
-
-    data = JSON.parse(data)
-    let count = Object.keys(data).length
-    data[`user${count+1}`] = req.body
-
-    res.end(JSON.stringify(data))
+app.delete('/deleteUser/:id', function (req, res) {
+  var id = parseInt(req.params.id)
+  fs.readFile(__dirname + "/" + "users.json", "utf8", function (err, dataFile) {
+    var data = JSON.parse(dataFile);
+    var convertData = Object.values(data);
+    var deletedData = convertData.filter(dd => dd.id !== id)
+    show={}
+    for(var i =0;i<deletedData.length;i++)
+    {
+      show[`user${deletedData[i].id}`]=deletedData[i];
+    }
+   res.send(show);
+    res.end();
   })
-})
+});
 
-var server = app.listen(8080, function(){
+
+var server = app.listen(8080, function () {
   var host = server.address().address
   var port = server.address().port
   console.log("Sample Code for RESTful API run at ", port)
 })
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
